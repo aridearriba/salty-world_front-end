e68e44ad790617e25ca8faa67df6ef1c9cd4efec
Ari de Arriba <aridease@gmail.com>
2021-02-12 16:44:44 +0100

    Correct relative paths

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 // Load log in custom button in navbar
 $(window).on("load", () => 
 {
-    let user = getLoggedUser();
+    let user = getLoggedUser(); 
+    let noIndex = '';
+       
+    if (!$("#index").length) noIndex = '.';
+
     if (user)
-        $("#loginButton").html(`<a id="loginButton" class="nav-link p-0" href="../pages/profile.html">
+        $("#loginButton").html(`<a id="loginButton" class="nav-link p-0" href="${noIndex}./pages/profile.html">
                                     <i class="fas fa-user pe-2"></i>
                                     Hola, ${user.name}
                                 </a>`);
     else
-        $("#loginButton").html(`<a id="loginButton" class="nav-link p-0" href="../pages/login.html">
+        $("#loginButton").html(`<a id="loginButton" class="nav-link p-0" href="${noIndex}./pages/login.html">
             <i class="fas fa-user pe-2"></i>
             Iniciar sessión
         </a>`);
